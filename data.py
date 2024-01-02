1b2498694982e7e3d91aba98dca408a50c660d39
bar00ng <salamanderttf@gmail.com>
2024-01-02 17:05:24 +0700

    WIP Quiz Page

---
+++ data.py
@@ -0,0 +1,47 @@
+data_planet = [
+    {
+        "nama" : "Matahari",
+        "image" : "./assets/planets/matahari.png",
+        "desc" : "Matahari adalah bola api besar yang memberikan cahaya dan panas ke Bumi seperti lampu raksasa di langit. Kita merasa hangat dan bisa melihat berkat matahari!"
+    },
+    {
+        "nama" : "Merkurius",
+        "image" : "./assets/planets/merkurius.png",
+        "desc" : "Planet yang paling dekat dengan matahari dan sangat panas. Seperti panci yang dipanaskan dengan sangat kuat."
+    },
+    {
+        "nama" : "Venus",
+        "image" : "./assets/planets/venus.png",
+        "desc" : "Planet yang penuh dengan awan dan sangat panas. Seperti oven yang menyala."
+    },
+    {
+        "nama" : "Bumi",
+        "image" : "./assets/planets/bumi.png",
+        "desc" : "Planet kita sendiri, yang memiliki air, tanah, dan udara. Tempat tinggal kita bersama segala jenis makhluk hidup."
+    },
+    {
+        "nama" : "Mars",
+        "image" : "./assets/planets/mars.png",
+        "desc" : "Planet yang memiliki warna kemerahan dan permukaan yang bergelombang. Seperti padang pasir yang luas."
+    },
+    {
+        "nama" : "Jupiter",
+        "image" : "./assets/planets/jupiter.png",
+        "desc" : "Planet terbesar di tata surya kita. Diliputi oleh awan besar dan memiliki banyak bulan. Seperti bola gas yang sangat besar."
+    },
+    {
+        "nama" : "Saturnus",
+        "image" : "./assets/planets/saturnus.png",
+        "desc" : "Planet yang terkenal dengan cincinnya yang indah. Seperti planet dengan hula-hoop di sekitarnya."
+    },
+    {
+        "nama" : "Uranus",
+        "image" : "./assets/planets/uranus.png",
+        "desc" : "Planet yang miring, berputar dengan posisi yang unik. Seperti bola yang berguling-guling."
+    },
+    {
+        "nama" : "Neptunus",
+        "image" : "./assets/planets/neptunus.png",
+        "desc" : "Planet yang jauh dan dingin, dengan badai besar di atmosfernya. Seperti bola yang diselimuti oleh awan gelap."
+    }
+]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from data import data_planet
 
 pygame.init()
 
@@ -11,6 +12,10 @@ FRAME = pygame.time.Clock()
 image_background = pygame.image.load('./assets/Background.jpg')
 BG = pygame.transform.scale(image_background, (WIDTH, HEIGHT))
 current_page = "welcome"
+wiki_index = 0
+ARROW_HEIGHT, ARROW_WIDTH = 30, 20
+PANEL_WIDTH, PANEL_HEIGHT = 300, 300
+MARGIN = 20
 
 # Game Variable
 username = 'Masukkan nama'
@@ -18,6 +23,47 @@ username = 'Masukkan nama'
 def display_text(text, size, color):
     FONT = pygame.font.Font('./assets/fonts/Press_Start_2P/PressStart2P-Regular.ttf', size)
     return FONT.render(text, False, color)
+
+def panel_text(text, font_size, color, max_width):
+    words = text.split()
+    lines = []
+    current_line = []
+
+    for word in words:
+        if current_line:
+            test_line = ' '.join(current_line + [word])
+        else:
+            test_line = word
+
+        line_width = pygame.font.Font('./assets/fonts/Press_Start_2P/PressStart2P-Regular.ttf', font_size).size(test_line)[0]
+
+        if line_width <= max_width:
+            current_line.append(word)
+        else:
+            lines.append(' '.join(current_line))
+            current_line = [word]
+
+    lines.append(' '.join(current_line))
+    return lines
+
+
+def draw_panel_text(text_lines, font_size, color, position, line_spacing):
+    font = pygame.font.Font('./assets/fonts/Press_Start_2P/PressStart2P-Regular.ttf', font_size)
+    line_height = font.get_linesize()
+
+    for i, line in enumerate(text_lines):
+        text_surface = font.render(line, True, color)
+        text_rect = text_surface.get_rect(topleft=(position[0], position[1] + i * (line_height + line_spacing)))
+        SCREEN.blit(text_surface, text_rect)
+        
+def draw_panel_and_text(text, font_size, color, max_width, panel_position):
+    panel_surface = pygame.image.load('./assets/misc/Panel.png')
+    panel_surface = pygame.transform.scale(panel_surface, (PANEL_WIDTH, PANEL_HEIGHT))
+    panel_rect = pygame.Rect(*panel_position, PANEL_WIDTH, PANEL_HEIGHT)
+    SCREEN.blit(panel_surface, panel_rect)
+
+    text_lines = panel_text(text, font_size, color, max_width)
+    draw_panel_text(text_lines, font_size, color, (panel_rect.topleft[0] + MARGIN, panel_rect.topleft[1] + MARGIN), 6)
 
 def welcome():
     # Display Text
@@ -62,6 +108,10 @@ def input_name():
     return button_rect
 
 def main_menu():
+    # Main menu button size
+    BUTTON_WIDTH = 125
+    BUTTON_HEIGHT = 65
+    
     # Display username in menu
     text_surface = display_text("Hello, " + username + "!", 16, "White")
     text_rect = text_surface.get_rect(topleft=(10, 10))
@@ -69,17 +119,120 @@ def main_menu():
     
     # Display Main Menu Text
     main_menu_surface = display_text("MAIN MENU", 30, "White")
-    main_menu_rect = main_menu_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 80))
+    main_menu_rect = main_menu_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 50))
     SCREEN.blit(main_menu_surface, main_menu_rect)
     
     # Wiki Button
+    wiki_button_surface = pygame.image.load('./assets/buttons/WikiButton.png')
+    wiki_button_surface = pygame.transform.scale(wiki_button_surface, (BUTTON_WIDTH, BUTTON_HEIGHT))
+    wiki_button_rect = wiki_button_surface.get_rect(center=(WIDTH // 2 - 150, HEIGHT // 2 + 20))
+    SCREEN.blit(wiki_button_surface, wiki_button_rect)
     
     # Quiz Button
+    quiz_button_surface = pygame.image.load('./assets/buttons/QuizButtons.png')
+    quiz_button_surface = pygame.transform.scale(quiz_button_surface, (BUTTON_WIDTH, BUTTON_HEIGHT))
+    quiz_button_rect = quiz_button_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 20))
+    SCREEN.blit(quiz_button_surface, quiz_button_rect)
     
     # About Button
+    about_button_surface = pygame.image.load('./assets/buttons/AboutButton.png')
+    about_button_surface = pygame.transform.scale(about_button_surface, (BUTTON_WIDTH, BUTTON_HEIGHT))
+    about_button_rect = about_button_surface.get_rect(center=(WIDTH // 2 + 150, HEIGHT // 2 + 20))
+    SCREEN.blit(about_button_surface, about_button_rect)
+    
+    return wiki_button_rect, quiz_button_rect, about_button_rect
 
 def about():
-    print("about")
+    # Display username in menu
+    text_surface = display_text("About", 24, "White")
+    text_rect = text_surface.get_rect(midtop=(WIDTH // 2, 20))
+    SCREEN.blit(text_surface, text_rect)
+    
+    # Home Button
+    home_button_surface = pygame.image.load('./assets/buttons/HomeButton.png')
+    home_button_surface = pygame.transform.scale(home_button_surface, (50, 50))
+    home_button_rect = home_button_surface.get_rect(topleft=(20,20))
+    SCREEN.blit(home_button_surface, home_button_rect)
+    
+    PANEL_WIDTH, PANEL_HEIGHT = 300, 150
+    
+    # Panel
+    panel_surface = pygame.image.load('./assets/misc/Panel.png')
+    panel_surface = pygame.transform.scale(panel_surface, (PANEL_WIDTH, PANEL_HEIGHT))
+    panel_rect = pygame.Rect(420, 125, PANEL_WIDTH, PANEL_HEIGHT)
+    SCREEN.blit(panel_surface, panel_rect)
+    
+    # Foto Jonathan
+    jo_image = pygame.image.load('./assets/misc/Jo.png').convert_alpha()
+    jo_image = pygame.transform.scale(jo_image, (225,225))
+    SCREEN.blit(jo_image, (150, 125))
+    
+    MARGIN = 20
+    
+    # Text - NAMA
+    nama_panel_text = display_text("Nama : Jonathan Vince", 12, "Black")
+    nama_panel_text_rect = nama_panel_text.get_rect(topleft=(panel_rect.topleft[0] + MARGIN, panel_rect.topleft[1] + MARGIN))
+    SCREEN.blit(nama_panel_text, nama_panel_text_rect)
+
+    # Text - KELAS
+    kelas_panel_text = display_text("Kelas : AA203", 12, "Black")
+    kelas_panel_text_rect = kelas_panel_text.get_rect(topleft=(nama_panel_text_rect.left, nama_panel_text_rect.bottom + MARGIN))
+    SCREEN.blit(kelas_panel_text, kelas_panel_text_rect)
+
+    # Text - NIM
+    nim_panel_text = display_text("NIM : 200030352", 12, "Black")
+    nim_panel_text_rect = nim_panel_text.get_rect(topleft=(kelas_panel_text_rect.left, kelas_panel_text_rect.bottom + MARGIN))
+    SCREEN.blit(nim_panel_text, nim_panel_text_rect)
+
+    # Text - BOD
+    bod_panel_text = display_text("BOD : 16 April 2002", 12, "Black")
+    bod_panel_text_rect = bod_panel_text.get_rect(topleft=(nim_panel_text_rect.left, nim_panel_text_rect.bottom + MARGIN))
+    SCREEN.blit(bod_panel_text, bod_panel_text_rect)
+
+    return home_button_rect
+
+def wiki(i):    
+    # Display username in menu
+    text_surface = display_text(data_planet[i]['nama'], 24, "White")
+    text_rect = text_surface.get_rect(midtop=(WIDTH // 2, 20))
+    SCREEN.blit(text_surface, text_rect)
+    
+    # Home Button
+    home_button_surface = pygame.image.load('./assets/buttons/HomeButton.png')
+    home_button_surface = pygame.transform.scale(home_button_surface, (50, 50))
+    home_button_rect = home_button_surface.get_rect(topleft=(20,20))
+    SCREEN.blit(home_button_surface, home_button_rect)
+    
+    # Previous Button
+    previous_button_surface = pygame.image.load('./assets/buttons/LeftArrow.png')
+    previous_button_surface = pygame.transform.scale(previous_button_surface, ( ARROW_WIDTH, ARROW_HEIGHT))
+    previous_button_rect = previous_button_surface.get_rect(midleft=(20, HEIGHT // 2))
+
+    if i > 0:
+        SCREEN.blit(previous_button_surface, previous_button_rect)
+
+    # Next Button
+    next_button_surface = pygame.image.load('./assets/buttons/RightArrow.png')
+    next_button_surface = pygame.transform.scale(next_button_surface, (ARROW_WIDTH, ARROW_HEIGHT))
+    next_button_rect = next_button_surface.get_rect(midright=(WIDTH - 20, HEIGHT // 2))
+
+    if i < (len(data_planet) - 1):
+        SCREEN.blit(next_button_surface, next_button_rect)
+        
+    draw_panel_and_text(data_planet[i]['desc'], 16, (0, 0, 0), PANEL_WIDTH - 2 * MARGIN, (400, 125))
+    
+    # Planet Image
+    planet_image_surface = pygame.image.load(data_planet[i]['image'])
+    # planet_image_surface = pygame.transform.scale(planet_image_surface,(250,250))
+    planet_image_rect = planet_image_surface.get_rect(center=(WIDTH // 2 - 150, HEIGHT // 2))
+    SCREEN.blit(planet_image_surface, planet_image_rect)
+
+
+    return home_button_rect, previous_button_rect, next_button_rect;
+
+
+def quiz():
+    print("Quiz")
     
 while True:
     for event in pygame.event.get():
@@ -103,6 +256,37 @@ while True:
                 if button_rect.collidepoint(event.pos):
                     main_menu()
                     current_page = "main_menu"
+            elif current_page == "main_menu":
+                main_menu_rects = main_menu()
+                wiki_rect, quiz_rect, about_rect = main_menu_rects
+                if wiki_rect.collidepoint(event.pos):
+                    wiki_index = 0
+                    wiki(wiki_index)
+                    current_page = "wiki"
+                elif quiz_rect.collidepoint(event.pos):
+                    quiz()
+                elif about_rect.collidepoint(event.pos):
+                    about()
+                    current_page = "about"
+            elif current_page == "about":
+                home_button_rect = about()
+                if home_button_rect.collidepoint(event.pos):
+                    main_menu()
+                    current_page = "main_menu"
+            elif current_page == "wiki":
+                wiki_rects = wiki(wiki_index)
+                home_button_rect, previous_button_rect, next_button_rect = wiki_rects
+                if home_button_rect.collidepoint(event.pos):
+                    main_menu()
+                    current_page = "main_menu"
+                if next_button_rect.collidepoint(event.pos) and wiki_index < (len(data_planet) - 1):
+                    wiki_index += 1
+                    wiki(wiki_index)
+                    current_page = "wiki"
+                if previous_button_rect.collidepoint(event.pos) and wiki_index > 0:
+                    wiki_index -= 1
+                    wiki(wiki_index)
+                    current_page = "wiki"
                     
     SCREEN.blit(BG, (0,0))  # Clear the screen
     
@@ -114,6 +298,8 @@ while True:
         main_menu()
     elif current_page == "about":
         about()
+    elif current_page == "wiki":
+        wiki(wiki_index)
                 
     pygame.display.update()
     FRAME.tick(60)
